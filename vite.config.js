dc51a029cd8762754ab6095560abb259c95a1149
Luisa Bravo <luisamaria.bravo@gmail.com>
2025-11-09 18:46:50 -0300

    Update base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TU_REPO/',   // 👈 exactamente el nombre del repo, con / al inicio y al final
+  base: '/https://marialu2016.github.io/fetch-en-react/',   // 👈 exactamente el nombre del repo, con / al inicio y al final
   plugins: [react()],
 })
