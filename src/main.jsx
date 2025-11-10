086d3a02dbe721f08e6561bae57bbc76ccc893c3
Luisa Bravo <luisamaria.bravo@gmail.com>
2025-11-09 22:06:20 -0300

    Replace BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 // import react
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 
-// import react router dom
-import { BrowserRouter } from 'react-router-dom';
 
 // import proyecto
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 )
