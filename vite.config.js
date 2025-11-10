e33f087049873bf700a1d07d30eeaee372ed2757
Luisa Bravo <luisamaria.bravo@gmail.com>
2025-11-09 23:30:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/https://marialu2016.github.io/fetch-en-react/',   
+  base: '/',   
   plugins: [react()],
 })
